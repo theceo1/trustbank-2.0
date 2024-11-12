0fbeb460c99879ee3801ac34da3cb973e1622e5f
theceo1 <loysiustony@hotmail.com.hk>
2024-11-12 09:23:53 +0100

    Add viewport meta tag and set input elements' font-size to prevent iOS zoom.

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" 
+        />
         <script defer data-domain="trustbank.tech" src="https://plausible.io/js/script.tagged-events.js" />
       </head>
       <body className={inter.className}>
